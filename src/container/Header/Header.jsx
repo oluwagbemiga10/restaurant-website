e6b484bc841d89f1d55a9b970d34693e9ce0672e
gbenga <oyelamigbenga11@gmail.com>
2024-02-25 00:53:53 +0100

    header section completed

--- src/container/Header/Header.jsx
+++ src/container/Header/Header.jsx
@@ -1,10 +1,23 @@
 import React from 'react';
-
+import {images} from "../../constants"
 import './Header.css';
+import { SubHeading } from '../../components';
 
 const Header = () => (
-  <div>
-    Header
+  <div className='app__header app__wrapper section__padding' id='#home'>
+  <div className="app__wrapper-info">
+  <SubHeading title="chase the new flavor" />
+  <h1 className='app__header-h1'>The key to fine Dining</h1>
+  <p className='p__opensans' style={{margin: "2rem 0"}}>
+
+  </p>
+  <button type='button' className='custom__button'>
+    Explore Menu
+  </button>
+  </div>
+  <div className="app__header-image">
+  <img src={images.welcome} alt='header image' />
+  </div>
   </div>
 );
 
