f9eb429ee77369cc41dad089475572aea0eaed31
gbenga <oyelamigbenga11@gmail.com>
2024-02-25 01:37:20 +0100

    added the header paragraph

--- src/container/AboutUs/AboutUs.jsx
+++ src/container/AboutUs/AboutUs.jsx
@@ -11,7 +11,9 @@ const AboutUs = () => (
       <div className="app__aboutus-content_about">
         <h1 className='headtext__cormorant'>About us</h1>
         <img src={images.spoon} alt='about__spoon' className='spoon__img' />
-        <p className='p__opensans'></p>
+        <p className='p__opensans'>
+        Lorem ipsum dolor sit amet, consectetur adipiscing elit. Quis pharetra adipiscing ultrices vulputate posuere tristique. In sed odio nec aliquet eu proin mauris et.
+        </p>
         <button type='button' className='custom__button'>Know more</button>
       </div>
       <div className="app__aboutus-content_knife flex__center">
@@ -20,7 +22,9 @@ const AboutUs = () => (
       <div className="app__aboutus-content_history">
         <h1 className='headtext__cormorant'>Our history</h1>
         <img src={images.spoon} alt='about__spoon' className='spoon__img' />
-        <p className='p__opensans'></p>
+        <p className='p__opensans'>
+        Adipiscing tempus ullamcorper lobortis odio tellus arcu volutpat. Risus placerat morbi volutpat habitasse interdum mi aliquam In sed odio nec aliquet.
+        </p>
         <button type='button' className='custom__button'>Know more</button>
       </div>
       </div>
